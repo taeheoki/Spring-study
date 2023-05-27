7860ae407294594f6ab89a9368d6a02aea867512
taeheoki <dkqjwl2365@naver.com>
2023-05-28 02:40:56 +0900

    🧑🏻‍💻feat: PK 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,20 +3,10 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity // JPA가 인식
-//@Table(name = "MBR")
-//@Table(uniqueConstraints = @UniqueConstraint(columnNames = "name"))
-//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
-@TableGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        table = "MY_SEQUENCES",
-        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
-)
 public class Member {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
-    @GeneratedValue(strategy = GenerationType.TABLE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false)
@@ -27,10 +17,6 @@ public class Member {
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getUsername() {
